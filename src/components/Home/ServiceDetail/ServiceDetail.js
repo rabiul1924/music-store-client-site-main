3f679159922f061c33fdea27729602debc2e3c89
Md.Rabiul islam <rabiulrabiul1924@gmail.com>
2021-04-17 21:56:19 +0600

    oo

--- src/components/Home/ServiceDetail/ServiceDetail.js
+++ src/components/Home/ServiceDetail/ServiceDetail.js
@@ -3,12 +3,12 @@ import './ServiceDetail.css'
 
 const ServiceDetail = ({ service }) => {
     return (
-        <div className="col-md-4 text-center body">
-            <img style={{height: '150px'}} src={service.imageURL} alt="" />
+        <div className="col-md-4 text-center body ">
+            <img  style={{height: '150px', margin:'15px', padding:'10px'}} src={service.imageURL} alt="" />
             <h5 className="mt-3 mb-3">{service.name}</h5>
             <h5 className="mt-3 mb-3">{service.price}Tk</h5>
             <p> {service.description}</p>
-          <button className="btn btn-danger">For Buy</button>
+          <button className="btn btn-danger mb-5">For Buy</button>
         </div>
     );
 };
