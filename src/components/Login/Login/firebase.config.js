27d98929f1b46b3aebfee4f1e290b629ed84b974
Md.Rabiul islam <66365150+rabiul1924@users.noreply.github.com>
2021-08-01 04:34:29 +0600

    Delete firebase.config.js

--- src/components/Login/Login/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyChjfk9Du5-o25DFqPclqHGVyp5ykCpIOc",
-  authDomain: "music-store-52da7.firebaseapp.com",
-  projectId: "music-store-52da7",
-  storageBucket: "music-store-52da7.appspot.com",
-  messagingSenderId: "467080295403",
-  appId: "1:467080295403:web:73ed0df61f6dc3b3a59fae"
-};
-  export default firebaseConfig;
